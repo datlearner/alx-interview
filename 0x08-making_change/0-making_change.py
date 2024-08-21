65cb07fca497b0d15c77823b3a7e5954b25a5589
datlearner <kundaimuchemwa456@gmail.com>
2024-08-21 18:41:52 +0200

    0x08. Making Change Algorithm Python

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/python3
+"""
+0-making_change.py
+"""
+
+
+def makeChange(coins, total):
+    """
+    Args: coins (list), total (int)
+    Returns: fewest coins needed (int)
+    """
+
+    if total <= 0:
+        return 0
+
+    number_coins = 0
+    coins.sort(reverse=True)
+
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            number_coins += 1
+
+    if total == 0:
+        return number_coins
+    return -1
+
