1601415ab59bde8bd91347270f6b83dbf6594fd2
datlearner <kundaimuchemwa456@gmail.com>
2024-08-21 18:47:09 +0200

    0x08. Making Change Algorithm Python

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,28 +1,22 @@
 
 #!/usr/bin/python3
-"""
-0-making_change.py
-"""
+"""Making Change Problem"""
 
 
 def makeChange(coins, total):
-    """
-    Args: coins (list), total (int)
-    Returns: fewest coins needed (int)
-    """
-
+    """Determines the fewest number of coins needed
+        to meet a given amount total"""
     if total <= 0:
         return 0
 
-    number_coins = 0
-    coins.sort(reverse=True)
-
+    current_total = 0
+    used_coins = 0
+    coins = sorted(coins, reverse=True)
     for coin in coins:
-        while coin <= total:
-            total -= coin
-            number_coins += 1
-
-    if total == 0:
-        return number_coins
+        r = (total - current_total) // coin
+        current_total += r * coin
+        used_coins += r
+        if current_total == total:
+            return used_coins
     return -1
 
